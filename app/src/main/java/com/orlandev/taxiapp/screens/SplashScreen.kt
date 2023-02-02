ae2c3c95008193ab28edbd524859810aca9220e1
Orlando N. Rodriguez <onovas05@gmail.com>
2023-02-02 17:17:20 -0500

    Initial commit

---
+++ app/src/main/java/com/orlandev/taxiapp/screens/SplashScreen.kt
@@ -0,0 +1,112 @@
+package com.orlandev.taxiapp.screens
+
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowForward
+import androidx.compose.material3.*
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import coil.compose.AsyncImage
+import com.orlandev.taxiapp.R
+import com.orlandev.taxiapp.ui.theme.TaxiAppTheme
+import com.orlandev.taxiapp.ui.theme.TaxiBlack
+import com.orlandev.taxiapp.ui.theme.TaxiGray
+import com.orlandev.taxiapp.ui.theme.TaxiYellow
+
+//TODO REVIEW
+val hPadding = 20.dp
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun SplashScreen() {
+
+    //Usaremos un Scaffold, de esta manera el FabButton queda en un lugar estandar
+    Scaffold(modifier = Modifier.fillMaxSize(), floatingActionButton = {
+        //Cambiaremos el FabButton de Material3 por el de Material2
+        //para que se vea igual al diseño original
+        androidx.compose.material.FloatingActionButton(
+            backgroundColor = TaxiYellow,
+            onClick = { /*TODO*/ }) {
+            Icon(imageVector = Icons.Default.ArrowForward, contentDescription = null)
+        }
+    }) { innerPadding ->
+        BoxWithConstraints(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(innerPadding)
+        ) {
+            AsyncImage(
+                model = R.drawable.background,
+                contentDescription = null,
+                modifier = Modifier.fillMaxSize(),
+                contentScale = ContentScale.Crop
+            )
+
+            Column(modifier = Modifier.fillMaxSize()) {
+                //Text
+                Spacer(modifier = Modifier.height(40.dp))
+                Column(
+                    modifier = Modifier
+                        .padding(horizontal = hPadding)
+                        .padding(top = 20.dp)
+                ) {
+                    Text(
+                        text = stringResource(id = R.string.welcome),
+                        style = MaterialTheme.typography.displayMedium,
+                        fontWeight = FontWeight.Bold,
+                        color = TaxiBlack,
+                        letterSpacing = 1.sp
+                    )
+                    Text(
+                        text = stringResource(id = R.string.subtitle),
+                        style = MaterialTheme.typography.titleLarge,
+                        fontWeight = FontWeight.Bold,
+                        color = TaxiGray
+                    )
+                }
+
+                //Imagen Central
+
+                AsyncImage(
+                    modifier = Modifier.fillMaxWidth(),
+                    model = R.drawable.middle_image,
+                    contentDescription = null
+                )
+
+                //Footer Text
+                Text(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(horizontal = hPadding)
+                        .padding(top = 10.dp),
+                    text = stringResource(id = R.string.splash_message),
+                    color = Color.White,
+                    fontWeight = FontWeight.Bold,
+                    fontSize = 22.sp,
+                    letterSpacing = 1.sp,
+                    lineHeight = 28.sp,
+                    style = MaterialTheme.typography.bodyMedium
+                )
+
+            }
+
+        }
+    }
+
+
+}
+
+@Preview()
+@Composable
+fun SplashScreenPreview() {
+    TaxiAppTheme {
+        SplashScreen()
+    }
+}
---
+++ app/src/main/java/com/orlandev/taxiapp/ui/theme/Color.kt
@@ -0,0 +1,17 @@
+package com.orlandev.taxiapp.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val TaxiYellow=Color(0xFFFED46C)
+val TaxiGray=Color(0xFF675B3D)
+val TaxiBlack=Color(0xFF393223)
+
+
+val Purple80 = Color(0xFFD0BCFF)
+val PurpleGrey80 = Color(0xFFCCC2DC)
+
+val Pink80 = Color(0xFFEFB8C8)
+val Purple40 = Color(0xFF6650a4)
+val PurpleGrey40 = Color(0xFF625b71)
+
+val Pink40 = Color(0xFF7D5260)
